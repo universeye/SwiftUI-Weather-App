3b8a4d8763ec622fb1a4c68f49c6df02a5629607
Terry <terrykuo501@gmail.com>
2021-03-26 17:03:26 +0800

    API Service + Forecast

---
+++ StewartWeather/Forecast.swift
@@ -0,0 +1,41 @@
+//
+//  Forecast.swift
+//  StewartWeather
+//
+//  Created by Terry Kuo on 2021/3/26.
+//
+
+import Foundation
+
+struct WeatherData: Decodable {
+    var daily: [Daily]
+}
+
+
+
+struct Daily: Decodable {
+    var dt: Date
+    var temp: Temp
+    var humidity: Int
+    var weather: [Weather]
+    var clouds: Int
+    var pop: Double
+}
+
+struct Temp: Decodable {
+    var day: Double
+    var min: Double
+    var max: Double
+}
+
+
+struct Weather: Codable{
+    var id: Int
+    var main: String
+    var description: String
+    var icon: String
+    var weatherIconUrl: URL {
+        let weatherIconUrlString = "http://openweathermap.org/img/wn/\(icon)@2x.png"
+        return URL(string: weatherIconUrlString)!
+    }
+}
