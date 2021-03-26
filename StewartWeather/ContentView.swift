40610cd74bd9769c29fc11a1c0d1ebc97e1e105a
Terry <terrykuo501@gmail.com>
2021-03-26 16:56:44 +0800

    Initial Commit

---
+++ StewartWeather/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  StewartWeather
+//
+//  Created by Terry Kuo on 2021/3/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
