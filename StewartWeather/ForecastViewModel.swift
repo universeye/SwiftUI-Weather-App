75a12f515a2ddbe208112c8ecbe41a05d4bcb5a3
Terry <terrykuo501@gmail.com>
2021-04-06 17:02:48 +0800

    Finish ForecastViewModel

---
+++ StewartWeather/ForecastViewModel.swift
@@ -0,0 +1,59 @@
+//
+//  ForecastViewModel.swift
+//  StewartWeather
+//
+//  Created by Terry Kuo on 2021/4/4.
+//
+
+import Foundation
+
+
+struct ForecastViewModel {
+    let forecast: Daily
+    
+    private static var dateFormatter: DateFormatter{
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "E, MM, d"
+        return dateFormatter
+    }
+    
+    private static var numberFormatter: NumberFormatter {
+        let numberFormatter = NumberFormatter()
+        numberFormatter.maximumFractionDigits = 0
+        return numberFormatter
+    }
+    
+    private static var numberFormatter2: NumberFormatter {
+        let numberFormatter = NumberFormatter()
+        numberFormatter.numberStyle = .percent
+        return numberFormatter
+    }
+    
+    var day: String {
+        return Self.dateFormatter.string(from: forecast.dt)
+    }
+    
+    var overview: String {
+        forecast.weather[0].description.capitalized
+    }
+    
+    var high: String {
+        return "H: \(Self.numberFormatter.string(for: forecast.temp.max) ?? "0")°"
+    }
+    
+    var low: String {
+        return "L: \(Self.numberFormatter.string(for: forecast.temp.min) ?? "0")°"
+    }
+    
+    var pop : String {
+        return "💧\(Self.numberFormatter2.string(for: forecast.pop) ?? "0%")"
+    }
+    
+    var clouds: String {
+        return "☁️\(forecast.clouds)%"
+    }
+    
+    var humidity: String {
+        return "Humidity: \(forecast.humidity)%"
+    }
+}
