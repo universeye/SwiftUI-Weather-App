b0dde83af5d6963d6f44a886a9fa77f004d0b468
Terry <terrykuo501@gmail.com>
2021-04-08 16:08:33 +0800

    Added ListingView to refactor contentView

--- StewartWeather/View/ContentView.swift
+++ StewartWeather/View/ContentView.swift
@@ -65,39 +65,13 @@ struct ContentView: View {
                         VStack(alignment:.leading) {
                             //List(0..<forecast.daily.count) { index in
                             List(forecastListVM.forecasts, id: \.day) { day in
-                                VStack(alignment:.leading) {
-                                    Text(day.day).bold()
-                                    HStack {
-                                        WebImage(url: day.weatherIconUrl)
-                                            .resizable()
-                                            .placeholder {
-                                                SFSymbols.load
-                                            }
-                                            .aspectRatio(contentMode: .fill)
-                                            .frame(width: 80, height: 80)
-                                            //.background(RoundedRectangle(cornerRadius: 10))
-                                        VStack (alignment:.leading) {
-                                            //Text("\(day.weather[0].description.capitalized)")
-                                            Text(day.overview)
-                                                .font(.title2)
-                                            HStack {
-                                                Text(day.high)
-                                                Text(day.low)
-                                            }
-                                            HStack {
-                                                Text(day.clouds)
-                                                Text(day.pop)
-                                            }
-                                            Text(day.humidity)
-                                        }
-                                    }
-                                }
+                                ListingView(day: day)
                             }
                             .listStyle(PlainListStyle())
                         }
                     }
                     .padding(.horizontal)
-                    .navigationTitle("Mobile Weather")
+                    .navigationTitle("DZWeather")
                     .navigationBarItems(trailing:
                                             Button(action: {
                                                 
---
+++ StewartWeather/View/ListingView.swift
@@ -0,0 +1,50 @@
+//
+//  ListingView.swift
+//  StewartWeather
+//
+//  Created by Terry Kuo on 2021/4/8.
+//
+
+import SwiftUI
+import SDWebImageSwiftUI
+
+struct ListingView: View {
+    
+    var day =  ForecastListViewModel().forecasts[0]
+    
+    var body: some View {
+        VStack(alignment:.leading) {
+            Text(day.day).bold()
+            HStack {
+                WebImage(url: day.weatherIconUrl)
+                    .resizable()
+                    .placeholder {
+                        SFSymbols.load
+                    }
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: 80, height: 80)
+                    //.background(RoundedRectangle(cornerRadius: 10))
+                VStack (alignment:.leading) {
+                    //Text("\(day.weather[0].description.capitalized)")
+                    Text(day.overview)
+                        .font(.title2)
+                    HStack {
+                        Text(day.high)
+                        Text(day.low)
+                    }
+                    HStack {
+                        Text(day.clouds)
+                        Text(day.pop)
+                    }
+                    Text(day.humidity)
+                }
+            }
+        }
+    }
+}
+
+struct ListingView_Previews: PreviewProvider {
+    static var previews: some View {
+        ListingView()
+    }
+}
